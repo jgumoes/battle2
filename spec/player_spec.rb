c74eb845cc2ef93fe52bdd36f1442c473646b609
Jack Gumoes <j.gumoes@gmail.com>
2021-02-19 15:37:23 +0000

    created Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+
+require "player"
+
+describe Player do
+    subject(:charlotte) { Player.new("Charlotte")}
+
+    describe "#name" do
+        it 'returns the name' do
+            expect(charlotte.name).to eq "Charlotte"
+        end
+    end
+end
